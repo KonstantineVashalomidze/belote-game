59525d38befe410c696f6b9138d80cb079044a42
KonstantineVashalomidze <vashalomidzekonstantine@gmail.com>
2026-05-30 22:04:07 +0400

    feat: finally

--- src/main/resources/static/js/api.js
+++ src/main/resources/static/js/api.js
@@ -32,6 +32,18 @@ export const bidSuitRound2 = (roomId, nickname, suit) =>
 export const bidSuitForced = (roomId, nickname, suit) =>
   post(`${API_BASE}/${roomId}/koziroba/vinujdeni`, { zedmetsaxeli: nickname, cveti: suit });
 
+export const playCard = (roomId, nickname, suit, rank, belote = 'ARAA_NACXADEBI') =>
+  post(`${API_BASE}/${roomId}/kartis-chamosvla`, { zedmetsaxeli: nickname, cveti: suit, ranki: rank, belotisCxadeba: belote });
+
+export const declareCombo = (roomId, nickname, kombinaciebi) =>
+  post(`${API_BASE}/${roomId}/kombinacia/cxadeba`, { zedmetsaxeli: nickname, kombinaciebi });
+
+export const readyForPlay = (roomId, nickname) =>
+  post(`${API_BASE}/${roomId}/kombinacia/mzadyofna`, { zedmetsaxeli: nickname });
+
+export const showCombos = (roomId, nickname) =>
+  post(`${API_BASE}/${roomId}/kombinacia/chveneba`, { zedmetsaxeli: nickname });
+
 export async function getGameState(roomId, nickname) {
   const res = await fetch(`${API_BASE}/${roomId}/mdgomareoba?zedmetsaxeli=${encodeURIComponent(nickname)}`);
   if (!res.ok) {
